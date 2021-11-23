a69fd9a691fe45be61cd048fecf292e9206db2bf
Ariful Islam <arifulislam161024@gmail.com>
2021-11-23 14:53:39 +0600

    Connect to mongo db

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 // middlewear
 app.use(cors());
 app.use(express.json());
+
+// mongodb uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env
+	.DB_PASS}@firstcluster.fhu8f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// connect to mongodb
+async function run() {
+	try {
+		await client.connect();
+		const database = client.db('codeaxes');
+		const usersCollection = database.collection('users');
+		console.log('Connected to MongoDB');
+	} finally {
+		// await client.close();
+	}
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
